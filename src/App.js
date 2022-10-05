50899559fd138fcaf6379e1d49dd971692cd3e81
OtienohOwinoh <coowinoh@gmail.com>
2022-10-05 23:22:04 +0300

    This commit defines the app routers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { Route, Routes} from 'react-router-dom';
+import BookList from './components/BookList';
+import BookDetails from './components/BookDetails';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Favorites from './components/Favorites';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path ="/" element={<BookList />} />
+        <Route path ="/book/:id" element={<BookDetails />} />
+        <Route path ="/favorites" element={<Favorites />} />
+      </Routes>
 
     </div>
   );
---
+++ src/components/BookDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import '../App.css'
+
+const BookDetails = () => {
+
+    return(
+        <div>BookDetails </div>
+    );
+}; 
+
+export default BookDetails

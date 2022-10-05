1df98296753d189f095ecdc6bd9960deea09973a
OtienohOwinoh <coowinoh@gmail.com>
2022-10-06 00:19:06 +0300

    This commit fetches data from an external API

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,11 +1,26 @@
-import React from "react";
-import '../App.css'
+import React, {useState, useEffect} from "react";
+import '../App.css'; 
+import  { API_URL} from '../Api'
+import axios from "axios";
 
 const BookList = () => {
 
+    const [books, setBooks] = useState([])
+
+
+    useEffect(() =>{
+        axios
+        .get(API_URL)
+        .then( res =>{
+            console.log(res.data)
+            setBooks (res.data)
+        })
+        .catch(err=>console.log(err))
+    }, [])
+
     return(
-        <div>BookList </div>
-    );
-}; 
+        <div className='book-list'>BookList </div>
+    )
+}
 
 export default BookList

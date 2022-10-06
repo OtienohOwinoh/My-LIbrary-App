3782411c53b2c8ea3c952d5c50545c36ef4aeb0b
OtienohOwinoh <coowinoh@gmail.com>
2022-10-06 14:05:22 +0300

    This commit parses id to URL to fetch book details data

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import '../App.css'
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import {BOOK_DETAILS_URL} from "../Api";
+
 
 const BookDetails = () => {
 
-    return(
-        <div>BookDetails </div>
-    );
+    const [ book, setBook] = useState ({});
+
+    const{ id } = useParams;
+
+    useEffect(() => {
+        axios.get (`${BOOK_DETAILS_URL}/${id}`)
+        .then (res => {
+            setBook(res.data);
+        }).catch(err=> console.log(err))
+ 
+    }, [id])
+
+    return  <div className="book-details">
+            <div className="book-image">
+                <h2>{book?.title}</h2>
+                <img src={book?.image_url } alt="#" />
+            </div>
+            <div>
+                <h2 className="book-description">Descripion</h2>
+                <p>{book?.description}</p>
+                <h2>Authors</h2>
+                <p>{book?.authors}</p>
+                <h2>Genres</h2>
+                <p>{book?.genres}</p>
+            </div>
+        </div>
+    
 }; 
 
 export default BookDetails
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,12 +3,15 @@ import '../App.css';
 import  { API_URL} from '../Api'
 import axios from "axios";
 import { useAppContext } from "./context/appContext";
+import { useNavigate } from "react-router-dom";
 
 const BookList = () => {
 
     const [books, setBooks] = useState([])
 
     const { favorites, addToFavorites, removeFromFavorites} = useAppContext();
+
+    const navigate = useNavigate();
 
     console.log ("favorites are", favorites)
 
@@ -33,19 +36,19 @@ const BookList = () => {
             {books.map ((book)=> (
                 <div key={book.id} className="book">
                     <div><h4>{book.title}</h4></div>
-                    <div><img src={book.image_url} alt="#" /></div>
+                    <div><img src={book.image_url} alt="#" onClick={() => navigate (`/books/${book.id}`)}/></div>
                     <div>
-                        { favoritesChecker(book.id) ? 
+                        { favoritesChecker(book.id) ? (
                          <button 
                          onClick={() => removeFromFavorites(book.id)}>Remove from Favorites
                          </button> 
-                         :
+              )  : (
                          <button 
                          onClick={() => addToFavorites(book)}>Add to Favorites
                          </button>
 
                         
-                    }
+                   ) }
                         
                     </div>
                  </div>

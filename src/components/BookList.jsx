b2c305db021c0f21ab5f048bd6c00318f42be35d
OtienohOwinoh <coowinoh@gmail.com>
2022-10-08 18:17:04 +0300

    This commit adds a post request

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from "react-router-dom";
 const BookList = () => {
 
     const [books, setBooks] = useState([])
+    const [searchList, setSearchList] = useState([])
 
     const { favorites, addToFavorites, removeFromFavorites} = useAppContext();
 
@@ -21,39 +22,83 @@ const BookList = () => {
 
     }
 
-
     useEffect(() =>{
         axios
         .get(API_URL)
         .then( res =>{
             console.log(res.data)
             setBooks (res.data)
+            setSearchList (res.data)
         })
         .catch(err=>console.log(err))
     }, [])
 
-    return <div className='book-list'>
-            {books.map ((book)=> (
-                <div key={book.id} className="book">
-                    <div><h3 className="book-title">{book.title}</h3></div>
-                    <div className="desc-image"><img src={book.image_url} alt="#" onClick={() => navigate (`/books/${book.id}`)}/></div>
-                    <div>
-                        { favoritesChecker(book.id) ? (
-                         <button 
-                         onClick={() => removeFromFavorites(book.id)}>Remove from Favorites
-                         </button> 
-              )  : (
-                         <button 
-                         onClick={() => addToFavorites(book)}>Add to Favorites
-                         </button>
 
-                        
-                   ) }
-                        
-                    </div>
-                 </div>
-            ))}
-        </div>
+        function searchBook(e) {
+
+            // Get typed value
+
+            let searchTerm = document.getElementsByTagName("input")[0].value.toLowerCase()
+
+            // Filter Searchlist by value
+
+            let searchResults = searchList.filter(item => item.title.toLowerCase()?.includes (searchTerm))
+
+            // Set books state to filtered items
+
+            setBooks(searchResults)
+
+        }
+
+
+
+    return(
+        
+    <div id="home-div">
+
+        <center>
+
+      
+             <form action="search">
+        <input type="text" name="search" id="search" placeholder="Search your favorite title" 
+        onKeyUp={(e) => searchBook(e)}/>
+               
+ </form>
+
+ </center>
+
+
+    <div className='book-list'>
+ 
+    {books.map ((book)=> (
+     <div key={book.id} className="book">
+         <div><h3 className="book-title">{book.title}</h3></div>
+         <div className="desc-image"><img src={book.image_url} alt="#" onClick={() => navigate (`/books/${book.id}`)}/></div>
+         <div>
+             { favoritesChecker(book.id) ? (
+              <button 
+              onClick={() => removeFromFavorites(book.id)}>Remove from Favorites
+              </button> 
+   )  : (
+              <button 
+              onClick={() => addToFavorites(book)}>Add to Favorites
+              </button>
+             
+        ) }
+             
+         </div>
+      </div>
+ ))}
+</div>
+
+
+    </div>
+
+    )
+   
+   
+   
+   
 }
 
 export default BookList
